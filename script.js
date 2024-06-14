6b05ffa92016ad6c47e2c8b8a47b4671ba9496d0
jessebaird <30557841+jessebaird@users.noreply.github.com>
2024-06-13 21:12:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const data = {
     "Jesse Smith": [
-        { "Rental 2" },
-        { "Shac" },
-        { "Rac" }
+        { busList: "Bus List 1", vehicle: "Rental 2" },
+        { busList: "Bus List 2", vehicle: "Shac" },
+        { busList: "Bus List 3", vehicle: "Rac" }
     ],
     "Kristi Johnson": [
-        { "Rac" },
-        { "Benny" },
-        { "Shac" }
+        { busList: "Bus List 4", vehicle: "Rac" },
+        { busList: "Bus List 5", vehicle: "Benny" },
+        { busList: "Bus List 6", vehicle: "Shac" }
     ],
     // Add more names and their corresponding data as needed
 };
+
 
 
     const nameDropdown = document.getElementById('nameDropdown');
