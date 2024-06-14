aa3737f8389b649c3b6f94a1f51c3036c40f4959
jessebaird <30557841+jessebaird@users.noreply.github.com>
2024-06-13 20:59:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const data = {
-"Adam Jun": [ {  " Rental 1" }, { " Rental 2" }, { " Rental 1" }, { " Rac" }, { " Benny" }, { " Rental 2" }, { " Shac" } ],
-"Addy Jeffrey": [ {  " Benny" }, { " Rac" }, { " Benny" }, { " Shac" }, { " Rac" }, { " Shac" }, { " Rac" } ],
-"Aine Staggs": [ {  " Rac" }, { " Benny" }, { " Rental 2" }, { " Rental 2" }, { " Shac" }, { " Rental 2" }, { " Benny" } ],
-"Alyssa Thompson": [ {  " Rac" }, { " Shac" }, { " Shac" }, { " Rac" }, { " Rac" }, { " Rental 2" }, { " Benny" } ],
+        "Adam Jun": [ {  " Rental 1" }, { " Rental 2" }, { " Rental 1" }, { " Rac" }, { " Benny" }, { " Rental 2" }, { " Shac" } ],
+        "Addy Jeffrey": [ {  " Benny" }, { " Rac" }, { " Benny" }, { " Shac" }, { " Rac" }, { " Shac" }, { " Rac" } ],
+        "Aine Staggs": [ {  " Rac" }, { " Benny" }, { " Rental 2" }, { " Rental 2" }, { " Shac" }, { " Rental 2" }, { " Benny" } ],
+        "Alyssa Thompson": [ {  " Rac" }, { " Shac" }, { " Shac" }, { " Rac" }, { " Rac" }, { " Rental 2" }, { " Benny" } ],
         // Add more names and their corresponding data as needed
     };
 
